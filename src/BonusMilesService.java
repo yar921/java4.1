e89b310bf59c1823fb814e4d057b4a9bfb78d5c8
Yaroslav.S <yar921@yandex.ru>
2022-09-11 20:59:02 +0300

    new

---
+++ src/BonusMilesService.java
@@ -0,0 +1,6 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int cost = (price / 20);
+        return cost;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
+
+        System.out.println();
+        System.out.println(service.calculate(1000));
+    }
+}
