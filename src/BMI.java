1396e8c31d260e7cfd8daaf9f3c38e3d0abd5bce
yar921 <99743746+yar921@users.noreply.github.com>
2022-09-13 10:48:03 +0300

    Update BMI.java

--- src/BMI.java
+++ src/BMI.java
@@ -1,11 +1,9 @@
 public class BMI {
     public static void main(String[] args) {
         BmiService servise = new BmiService();
-        int h = 170;
-        int m = 70;
-        double index = servise.calculate(m, h);
+        double index = servise.calculate(70, 1.72);
 
-        System.out.println(servise.calculate(77, 1.7)+ " индекс массы тела");
+        System.out.println("Индекс массы тела: " + index);
 
     }
 }
