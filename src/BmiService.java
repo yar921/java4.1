0d17f1fe50095e908f16c1134caed0c4b9c1b170
yar921 <99743746+yar921@users.noreply.github.com>
2022-10-08 09:27:06 +0300

    Delete BmiService.java

--- src/BmiService.java
+++
@@ -1,7 +0,0 @@
-public class BmiService {
-    public double calculate(int m, double h){
-        double height = h * h;
-        double index = m / height;
-        return (double) index;
-    }
-}
