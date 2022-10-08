86dfc63732fab146cb74d3beb095b847ad8cd4bf
yar921 <99743746+yar921@users.noreply.github.com>
2022-10-08 09:26:58 +0300

    Delete BMI.java

--- src/BMI.java
+++
@@ -1,9 +0,0 @@
-public class BMI {
-    public static void main(String[] args) {
-        BmiService servise = new BmiService();
-        double index = servise.calculate(70, 1.72);
-
-        System.out.println("Индекс массы тела: " + index);
-
-    }
-}
